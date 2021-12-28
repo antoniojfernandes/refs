6ba916e5e4eab8d0cef8e180fae4aef4de25c437
António Fernandes <antoniojfernandes@outlook.pt>
2021-12-28 20:51:04 +0000

    initial commit

---
+++ src/main/java/vetsobrerodas/refs/RefsApplication.java
@@ -0,0 +1,13 @@
+package vetsobrerodas.refs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RefsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/vetsobrerodas/refs/RefsApplicationTests.java
@@ -0,0 +1,13 @@
+package vetsobrerodas.refs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
