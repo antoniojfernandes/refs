0247734c5d78080f1cec33facb1e122d730a33e3
António Fernandes <antoniojfernandes@outlook.pt>
2022-01-05 11:34:51 +0000

    - Small fix to remove redundant code

--- src/main/java/vetsobrerodas/refs/Utils/IdGenerator.java
+++ src/main/java/vetsobrerodas/refs/Utils/IdGenerator.java
@@ -8,9 +8,9 @@ import java.util.Random;
 public class IdGenerator{
 
     private static final char[] lettersAndNumbers = {
-            'A','B','C','D','E','F','G','H','I','J','K','L',
-            'M','N','O','P','Q','R','S','T','U','V','X','Y',
-            'Z','0','1','2','3','4','5','6','7','8','9'
+            'A','B','C','D','E','F','G','H','J','K',
+            'M','N','P','Q','R','S','T','U','V','X','Y',
+            'Z','1','2','3','4','5','6','7','8','9'
     };
 
 
@@ -32,8 +32,5 @@ public class IdGenerator{
         return idAsString;
     }
 
-    private boolean contains(String id){
-        return usedSequences.contains(id);
-    }
 }
 
